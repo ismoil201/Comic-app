dc8d34aa303fe568d63a96754984618de32a1ce0
ismoil <ismoiljoraxonov1@gmail.com>
2025-11-06 22:28:58 +0900

    feat: add WebView to load Naver Webtoon website with JavaScript support

--- app/src/main/java/com/example/webtoon/MainActivity.kt
+++ app/src/main/java/com/example/webtoon/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.webtoon
 
 import android.os.Bundle
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.webtoon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.webView.apply {
+            loadUrl("https://comic.naver.com/webtoon")
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+
         }
+
+
+
     }
 }
